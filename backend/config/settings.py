b47458e4ce6d4ddba3d26a92c7362b7dfc503b3f
Mugtaba <mojtaba.mohamed187@gmail.com>
2025-04-04 22:52:34 +0200

    fix: load secret_key from environment variables

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2a%sn2ukdi2ao^!w6h(vjr8@)c^qqpp0ty^lrrv5^eu7p$*n$w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
