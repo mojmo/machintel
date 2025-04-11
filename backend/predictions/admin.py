f345197bc1065a8795adcd6da0515d6e01ee774e
Makkawy-Alnouman <makkawyalnouman@gmail.com>
2025-04-11 18:28:32 +0300

    Remove backend/predictions folder from Git tracking

--- backend/predictions/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import PredictionResult  # If you have models
-
-admin.site.register(PredictionResult)
--- backend/predictions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PredictView, ModelInfoView
-
-urlpatterns = [
-    path('predict/', PredictView.as_view(), name='predict'),
-    path('model-info/', ModelInfoView.as_view(), name='model-info'),
-]
